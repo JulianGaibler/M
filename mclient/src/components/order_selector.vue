fda1c736eee3de3a4bbbf6ba716ff4301dfc64f5
JulianWels <julian.social.online@gmail.com>
2017-11-15 01:36:21 +0100

    Minor Changes

--- mclient/src/components/order_selector.vue
+++ mclient/src/components/order_selector.vue
@@ -1,11 +1,20 @@
 <template> 
 	<div class="whitebox">
-		<draggable :list="items" @change="dragMoved" :options="{handle:'.handle'}">
+		<draggable :list="items" :options="{handle:'.handle'}">
 		<div v-for="item in items" :class="[$style.elem]">
 			<div :class="['handle',$style.action]">
 				<icon :class="$style.dragsvg" :svg="drag_handle"></icon>
 			</div>
 			<div>{{item}}</div>
+			<div>
+			<div class="mdc-switch">
+  <input type="checkbox" id="basic-switch" class="mdc-switch__native-control" />
+  <div class="mdc-switch__background">
+    <div class="mdc-switch__knob"></div>
+  </div>
+</div>
+<label for="basic-switch" class="mdc-switch-label">off/on</label>
+			</div>
 		</div>
 		</draggable>
 	</div>
@@ -22,12 +31,19 @@ export default {
 		let todo = ['appetizers','salads','soups','promotions','maindishes','sidedishes','desserts'];//TODO
 		return {
 			drag_handle: require('./../assets/drag_handle.svg'),
-			items: todo
+			items: this.convertNames(todo)
 		}
 	},
 	methods: {
 		getRandomArbitrary: function (min, max) {
 			return Math.random() * (max - min) + min;
+		},
+		convertNames: function(arr) {
+			for (var i = 0; i < arr.length; i++) {
+				if (this.$te('menuSection.'+arr[i]))
+					arr[i] = this.$t('menuSection.'+arr[i]);
+			}
+			return arr;
 		}
 	},
 	components: {
